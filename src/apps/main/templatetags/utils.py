4361f1a4f74317327e97215363a96db933017192
LEKS <ilhanchanel20002@gmail.com>
2025-12-04 16:20:16 +0300

    feature: home page faq & reviews

--- src/apps/main/templatetags/utils.py
+++ src/apps/main/templatetags/utils.py
@@ -13,3 +13,21 @@ def get_site_settings():
 @register.simple_tag
 def get_excursion_list():
     return models.Excursion.objects.all()
+
+
+@register.simple_tag
+def get_review_list():
+    return models.Review.objects.all()
+
+
+@register.simple_tag
+def get_faq_list():
+    return models.FAQ.objects.all()
+
+
+@register.filter
+def index(list_obj, i):
+    try:
+        return list_obj[int(i)]
+    finally:
+        return ""
